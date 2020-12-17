585c838c366daf0455a1ca3bc96aef4dff5c077a
Puhi <tomee1992@gmail.com>
2020-12-17 14:28:28 +0100

    Update projektu

--- src/com/pluhi/Student.java
+++ src/com/pluhi/Student.java
@@ -1,75 +1,80 @@
 package com.pluhi;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Random;
 
 public class Student {
-    Map<Integer, String> znamky = new HashMap<Integer, String>(); //sposob ako pridat k znamkam aj predmet
+    Map<String, ArrayList<Integer>> znamky = new HashMap<>();
 
     private final int rocnik;
     private final String meno;
-    private int znamka;
-
-    //bolean jeBakalar
-    //bolean jeInzinier
 
 
     public Student(String meno, int rocnik) {
         this.meno = meno;
         this.rocnik = rocnik;
-        //kontrola mena a rocnika
 
+    }
+
+    public boolean stupenStudenta() {
+        if (rocnik > 0 && rocnik < 4) {
+            System.out.println("Student " + meno + " je bakalar.");
+        } else if (rocnik > 3 && rocnik <= 5) {
+            System.out.println("Student " + meno + " je inzinier.");
+        } else
+            System.out.println("Rocnik je neznamy.");
+        return true;
     }
 
     public void vypis() {
-        System.out.printf("Meno studenta: %s%n%s%s%n", meno, "Rocnik: ", rocnik);
+        System.out.println("Meno studenta: " + meno);
+        System.out.println("Rocnik: " + rocnik);
+        System.out.println(stupenStudenta());
 
-        if (rocnik > 0 && rocnik < 4)
-            System.out.println(meno + " je bakalar");
+    }
 
-        if (rocnik > 3 && rocnik < 6) {
-            System.out.println(meno + " je inzinier");
-
+    public void vypisZnamky() {
+        for (Map.Entry<String, ArrayList<Integer>> entry : znamky.entrySet()) {
+            System.out.print(entry.getKey() + " | ");
+            for (int predmZ : entry.getValue()) {
+                System.out.print(predmZ + " ");
+            }
+            System.out.println();
         }
     }
 
-    public boolean vlozZnamku(int znamka) {
-        //over ci dany predmet existuje v danom mape
-        //ked neexistuje musim vlozit novy zaznam
+    public void vlozZnamky(String predmet, int pocetZnamok) {
 
-        if (znamka > 0 && znamka < 6) {
-            znamky.put(1, "Matematika"); //HashMap put
-            return true;
-        } else {
-            System.out.println("Neplatny rozsah");
-            return false;
+        Random rn = new Random();
+        ArrayList<Integer> udeleneZnamky = new ArrayList<>();
+        for (int i = 0; i < pocetZnamok; i++) {
+            udeleneZnamky.add(rn.nextInt(5) + 1); //s tymto zabezpecujem abz znamky boli z intervalu 1-5
         }
+        znamky.put(predmet, udeleneZnamky);
 
-//        return znamka <= 5;
     }
 
-    public void vypisVsetkyZnamky() {
+    public double vypocitajPriemer(String predmet) {
+        double sum = 0;
+        double priemer = 0;
 
+        for (Integer number : this.znamky.get(predmet)) {
+            sum += number;
+        }
 
-//        znamky.add(0, znamka);
-//        znamky.add(1, znamka);
-//        znamky.add(2, znamka);
-//        znamky.add(3, znamka);
-//        znamky.add(4, znamka);
-//        znamky.add(5, znamka);
-//        znamky.add(6, znamka);
-//        znamky.add(7, znamka);
-//        znamky.add(8, znamka);
-//        znamky.add(9, znamka);
-//
-        //for cyklus
+        if (this.znamky.get(predmet).size() != 0) priemer = sum / this.znamky.get(predmet).size();
 
-
-        System.out.print(znamky + "  \n\n");
+        return priemer;
     }
 
 }
+
+
+
+
+
 
 
 
--- src/com/pluhi/StudentTester.java
+++ src/com/pluhi/StudentTester.java
@@ -3,23 +3,36 @@ package com.pluhi;
 public class StudentTester {
     public static void main(String[] args) {
 
-        Student student = new Student("Peter Studeny", 4);
+        Student student = new Student("Peter Studeny", 2);
         student.vypis();
 
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(5));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(5));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(5));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(4));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(3));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(5));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(1));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(2));
-        System.out.println("Znamka je z intervalu 1-5: " + student.vlozZnamku(4));
+        student.vlozZnamky("Matematika", 10);
+        student.vlozZnamky("Chemia", 10);
+        student.vlozZnamky("Biologia", 10);
+        student.vlozZnamky("Fyzika", 10);
+        System.out.println("\nUdelene znamky studenta: ");
+        student.vypisZnamky();
+        System.out.println("Priemer znamok z matematiky : " + student.vypocitajPriemer("Matematika"));
+        System.out.println("Priemer znamok z chemie: " + student.vypocitajPriemer("Chemia"));
+        System.out.println("Priemer znamok z biologie: " + student.vypocitajPriemer("Biologia"));
+        System.out.println("Priemer znamok z fyziky: " + student.vypocitajPriemer("Fyzika"));
 
+        System.out.println("--------------------------------");
 
-
-        Student student1 = new Student("Karol Novak", 1);
+        Student student1 = new Student("Karol Novak", 5);
         student1.vypis();
+
+        student1.vlozZnamky("Matematika", 10);
+        student1.vlozZnamky("Chemia", 10);
+        student1.vlozZnamky("Biologia", 10);
+        student1.vlozZnamky("Fyzika", 10);
+        System.out.println("\nUdelene znamky studenta: ");
+        student1.vypisZnamky();
+        System.out.println("Priemer znamok z matematiky : " + student1.vypocitajPriemer("Matematika"));
+        System.out.println("Priemer znamok z chemie: " + student1.vypocitajPriemer("Chemia"));
+        System.out.println("Priemer znamok z biologie: " + student1.vypocitajPriemer("Biologia"));
+        System.out.println("Priemer znamok z fyziky: " + student1.vypocitajPriemer("Fyzika"));
+
 
     }
 }
